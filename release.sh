a2d7e42e82a3ce35d639eef1c70640df0b17353d
Andres Viedma <andres.viedma@gmail.com>
2025-07-12 04:22:36 +0200

    Script to release

---
+++ release.sh
@@ -0,0 +1,24 @@
+last_version=$(grep -i -E "^trekkieVersion[[:space:]]*=" gradle.properties | cut -d'=' -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+
+echo "*** Last version: $last_version"
+echo "*** Please enter new version"
+read new_version
+
+sed -i -E "/^trekkieVersion[[:space:]]*=/c\\
+trekkieVersion=$new_version
+" gradle.properties
+
+echo "**** updated gradle.properties"
+git diff gradle.properties
+
+read -p "Are you sure you want to release this version? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    ./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication
+
+    git commit -m "Release $new_version" gradle.properties
+    git tag -a "v$new_version" -m "Version $new_version"
+    git push
+    git push origin "v$new_version"
+fi
